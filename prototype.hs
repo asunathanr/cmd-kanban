135b786d132c53bf4d0009e46c7a57ac421594ac
Nathan Robertson <absnathanr@gmail.com>
2019-02-08 08:04:26 -0600

    Added starting eval command

--- prototype.hs
+++ prototype.hs
@@ -8,4 +8,15 @@ main = loop
 loop = do
   putStr "> "
   input <- getLine
-  putStrLn input
+  return (eval input)
+
+eval "help" = help
+eval unknowncmd = defhandler
+
+help = "Help section for cmd-kanban."
+
+defhandler = "Error: unrecognized command."
+
+-- Keep reading input unless user enters quit.
+--nextline "quit" = Nothing
+--nextline cmd = Just loop
